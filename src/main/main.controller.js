9e5674fa77e2ccbb02fe487bd67c3dc15f901e5d
Kevin Altman <kevin@itsthatguy.com>
2015-05-18 09:30:25 -0400

    adds default text for skills

--- src/main/main.controller.js
+++ src/main/main.controller.js
@@ -1,8 +1,26 @@
 'use strict';
 
 function MainCtrl($rootScope, $scope, SkillsService, DataService, BuildsService, OverlayService, ModalService) {
-  SkillsService.get().then(function(data) {
+  $scope.build = {
+    name: "Nada",
+    class: "Your Mom",
+    active: [
+      {"id": 0, "skill": "Nothing Selected", "rune": "--"},
+      {"id": 1, "skill": "Nothing Selected", "rune": "--"},
+      {"id": 2, "skill": "Nothing Selected", "rune": "--"},
+      {"id": 3, "skill": "Nothing Selected", "rune": "--"},
+      {"id": 4, "skill": "Nothing Selected", "rune": "--"},
+      {"id": 5, "skill": "Nothing Selected", "rune": "--"}
+    ],
+    passive: [
+      {"id": 0, "skill": "Nothing Selected"},
+      {"id": 1, "skill": "Nothing Selected"},
+      {"id": 2, "skill": "Nothing Selected"},
+      {"id": 3, "skill": "Nothing Selected"}
+    ]
+  }
 
+  SkillsService.get().then(function(data) {
     BuildsService.get(1).then(function(data) {
       $scope.build = data;
     });
